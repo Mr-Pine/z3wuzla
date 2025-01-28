2b508a6a922feeff85d974f6e4f171f2e7d8592a
Mr-Pine <git@mr-pine.de>
2025-01-28 22:21:05 +0100

    Well, doesn't support integers so maybe another time

--- src/z3wuzla/__init__.py
+++ src/z3wuzla/__init__.py
@@ -1,2 +1,19 @@
-def main() -> None:
-    print("Hello from z3wuzla!")
+import z3
+import bitwuzla
+print("Hello from z3wuzla!")
+
+tm = bitwuzla.TermManager()
+options = bitwuzla.Options()
+options.set(bitwuzla.Option.PRODUCE_MODELS, True)
+parser = bitwuzla.Parser(tm, options)
+
+def wuzl_check(solver: z3.Solver, *assumptions):
+    if len(assumptions) > 0:
+        print("Assumptions are currently unsupported")
+    print("I wuzled your check")
+    smt2string = solver.to_smt2()
+    parser.parse(smt2string, True, False)
+
+    return parser.bitwuzla().check_sat()
+
+z3.Solver.check = wuzl_check
